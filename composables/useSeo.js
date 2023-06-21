d7e0d9bf13ef9db7873c6909e30ef6adc54b5701
daniele.falchetti2 <daniele.falchetti@viewnext.com>
2023-06-22 00:40:18 +0200

    we will prey

--- composables/useSeo.js
+++ composables/useSeo.js
@@ -4,23 +4,31 @@ export const useSeo = () => {
   const seo = story => {
     const config = useRuntimeConfig();
     const route = { origin: config.public.origin, ...useRoute() };
-    return {
+    useHead({
+      link: [
+        {
+          rel: 'canonical',
+          href: `${route.origin}${route.path}`
+        }
+      ]
+    });
+    useSeoMeta({
       title: `${story.content?.title ?? story.name} - ${enums.name}`,
       ...(story.content != null
         ? {
             description: story.content.intro,
             ogTitle: story.content.title,
             ogDescription: story.content.intro,
-            ogImage: story.content.file?.filename ?? undefined,
+            ogImage: story.content.file?.filename ?? story.content.image?.filename ?? undefined,
             ogUrl: `${route.origin}${route.path}`,
             twitterSite: `${route.origin}${route.path}`,
             twitterTitle: story.content.title,
-            twitterImage: story.content.file?.filename ?? undefined
+            twitterImage: story.content.file?.filename ?? story.content.image?.filename ?? undefined
           }
         : {
             description: story.description
           })
-    };
+    });
   };
   return { seo };
 };
