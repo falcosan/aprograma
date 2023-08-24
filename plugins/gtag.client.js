87e637b81d65af47a9ba61d57e68759527cc238d
daniele.falchetti@viewnext.com <daniele.falchetti@viewnext.com>
2023-08-24 13:44:16 +0200

    as

--- plugins/gtag.client.js
+++
@@ -1,9 +0,0 @@
-export default defineNuxtPlugin(() => {
-  const config = useRuntimeConfig();
-  window.dataLayer = window.dataLayer || [];
-  function gtag() {
-    dataLayer.push(arguments);
-  }
-  gtag('js', new Date());
-  gtag('config', config.public.envGTagId);
-});
