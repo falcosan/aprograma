7d3353b6fc6a195cd7bbce8edda39d5694a7b1b2
daniele.falchetti@viewnext.com <daniele.falchetti@viewnext.com>
2023-10-26 19:28:25 +0200

    s

--- server/middleware/cors.ts
+++ server/middleware/cors.ts
@@ -1,12 +1,16 @@
 export default defineEventHandler(event => {
+  const config = useRuntimeConfig();
   const headers = {
-    'Access-Control-Allow-Origin': 'Same-Origin',
-    crossOriginResourcePolicy: 'same-origin',
-    crossOriginOpenerPolicy: 'same-origin',
-    crossOriginEmbedderPolicy: 'require-corp',
-    contentSecurityPolicy:
-      "default-src 'self';base-uri 'self';font-src 'self' https: data:;form-action 'self';frame-ancestors 'self';img-src 'self' data:;object-src 'none';script-src 'self';script-src-attr 'none';style-src 'self' https: 'unsafe-inline';upgrade-insecure-requests",
-    'X-XSS-Protection': 1
+    'X-XSS-Protection': 1,
+    'Cross-Origin-Opener-Policy': 'same-origin',
+    'Cross-Origin-Resource-Policy': 'same-origin',
+    'Cross-Origin-Embedder-Policy': 'require-corp',
+    'Access-Control-Allow-Origin': [
+      config.public.envDomain,
+      config.public.envDomain.replace(/\/$/, '')
+    ],
+    'Content-Security-Policy':
+      "base-uri 'self'; object-src 'none'; form-action 'self'; frame-ancestors 'self'; upgrade-insecure-requests; font-src 'self' https: data:; img-src * 'self' https: data:; style-src 'self' https: 'unsafe-inline'; script-src 'self' https: 'unsafe-inline'; script-src-attr 'self' https: 'unsafe-inline';"
   };
   setHeaders(event, headers);
 });
