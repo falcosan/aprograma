b22507831f7cf4903824b6411e439bce25c7f641
daniele.falchetti@viewnext.com <daniele.falchetti@viewnext.com>
2023-10-26 16:03:36 +0200

    sa

--- server/api/storyblok.get.ts
+++ server/api/storyblok.get.ts
@@ -4,9 +4,10 @@ import { fetchStoryblok } from '@/services/fetch';
 export default defineEventHandler(async event => {
   const query = getQuery(event);
   const config = useRuntimeConfig();
+  const host = getRequestHeader(event, 'host');
   const auth = getResponseHeader(event, 'x-auth');
-  const cors = getResponseHeader(event, 'Access-Control-Allow-Origin');
-  if (auth === config.envXAuth && Array.isArray(cors) && cors?.includes(config.public.envDomain)) {
+  const cors = getResponseHeader(event, 'access-control-allow-origin');
+  if (auth === config.envXAuth && Array.isArray(cors) && new RegExp(host || '').test(cors[0])) {
     const slug = query.slug ?? '';
     const language = query.lang as ISbStoriesParams['language'];
     const startsWith = query.starts_with as ISbStoriesParams['starts_with'];
