2314ac503f07cb17c1510b3dea65cb3e64ea9cd5
daniele.falchetti@viewnext.com <daniele.falchetti@viewnext.com>
2023-10-26 17:19:30 +0200

    w

--- server/api/storyblok.get.ts
+++ server/api/storyblok.get.ts
@@ -4,8 +4,9 @@ import { fetchStoryblok } from '@/services/fetch';
 export default defineEventHandler(async event => {
   const query = getQuery(event);
   const config = useRuntimeConfig();
+  const host = getRequestHeader(event, 'host');
   const auth = getResponseHeader(event, 'x-auth');
-  if (auth === config.envXAuth) {
+  if (auth === config.envXAuth && host && new RegExp(host).test(config.public.envDomain)) {
     const slug = query.slug ?? '';
     const language = query.lang as ISbStoriesParams['language'];
     const startsWith = query.starts_with as ISbStoriesParams['starts_with'];
