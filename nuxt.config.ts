1e4d2d8dbb82a92ca0f9a22a1b1c1094fc307822
daniele.falchetti@viewnext.com <daniele.falchetti@viewnext.com>
2023-11-02 09:29:57 +0100

    wa

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -60,6 +60,7 @@ export default defineNuxtConfig({
     detectBrowserLanguage: false
   },
   security: {
+    nonce: true,
     headers: {
       xXSSProtection: '1',
       crossOriginEmbedderPolicy: 'unsafe-none',
@@ -71,9 +72,9 @@ export default defineNuxtConfig({
         'upgrade-insecure-requests': true,
         'font-src': ["'self'", 'https:', 'data:'],
         'img-src': ['*', "'self'", 'https:', 'data:'],
-        'style-src': ["'self'", 'https:', "'unsafe-inline'"],
-        'script-src': ["'self'", 'https:', "'unsafe-inline'"],
-        'script-src-attr': ["'self'", 'https:', "'unsafe-inline'"]
+        'script-src': ["'self'", "'nonce-{{nonce}}'", "'strict-dynamic'"],
+        'script-src-attr': ["'self'", "'nonce-{{nonce}}'", "'strict-dynamic'"],
+        'style-src': ["'self'", isProduction ? "'nonce-{{nonce}}'" : "'unsafe-inline'"]
       }
     }
   },
