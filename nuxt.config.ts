4a9cb2d7361d7c3a8e04b1d123e4646ccdea30c6
daniele.falchetti@viewnext.com <daniele.falchetti@viewnext.com>
2023-11-09 13:41:50 +0100

    d

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -85,7 +85,7 @@ export default defineNuxtConfig({
     includeAssets: ['favicon.ico'],
     workbox: {
       navigateFallback: null,
-      globPatterns: ['**/*.{js,json,ico,html,png,js,txt,css,svg}'],
+      globPatterns: ['**/*.{js,css}'],
       globIgnores: ['sw.js', 'workbox-*.js']
     }
   },
