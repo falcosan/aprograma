5fa25f544562f98cb589bd166e14f30e6da09f53
daniele.falchetti@viewnext.com <daniele.falchetti@viewnext.com>
2023-11-09 15:29:10 +0100

    fa

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -84,6 +84,7 @@ export default defineNuxtConfig({
     registerType: 'autoUpdate',
     workbox: {
       globDirectory: 'dist',
+      directoryIndex: 'dist',
       navigateFallback: null,
       globIgnores: ['sw.js', 'workbox-*.js'],
       globPatterns: [
