1b66cb897d473f569bc3b210598472f2d9b46e58
daniele.falchetti2 <daniele.falchetti@viewnext.com>
2023-11-13 00:27:59 +0100

    f

--- modules/prerenderRoutes.ts
+++ modules/prerenderRoutes.ts
@@ -1,19 +1,20 @@
+import { isProduction } from 'std-env';
 import { defineNuxtModule, addPrerenderRoutes } from '@nuxt/kit';
 import enums from '../utils/enums';
 import { fetchStories } from '../utils/fetch';
 export default defineNuxtModule({
   async setup() {
+    const envProductionDomain =
+      isProduction && !/netlify/gm.test(process.env.NUXT_ENV_DOMAIN || 'netlify');
     const staticRoutes = [
       '/',
-      '/robots.txt',
       `/${enums.sitemap}`,
-      ...Object.values(enums.rss)
-        .map(item => {
-          if (item instanceof Object) return `/${item.path}`;
-          else return '';
-        })
-        .filter(Boolean)
-    ];
+      envProductionDomain ? '/robots.txt' : null,
+      ...Object.values(enums.rss).map(item => {
+        if (item instanceof Object) return `/${item.path}`;
+        else return '';
+      })
+    ].filter(Boolean);
     const dynamicRoutes = await fetchStories();
     const routes = [...staticRoutes, ...dynamicRoutes];
     addPrerenderRoutes(routes);
