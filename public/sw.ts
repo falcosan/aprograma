9e98cafcb0bfca708fd24bcedfb407335586a1c9
daniele.falchetti2 <daniele.falchetti@viewnext.com>
2023-11-12 19:15:10 +0100

    l

--- public/sw.ts
+++ public/sw.ts
@@ -1,20 +1,6 @@
 import { clientsClaim } from 'workbox-core'
-import { registerRoute, NavigationRoute } from 'workbox-routing';
-import { precacheAndRoute, cleanupOutdatedCaches, createHandlerBoundToURL } from 'workbox-precaching';
 
 declare let self: ServiceWorkerGlobalScope
 
 self.skipWaiting();
 clientsClaim();
-precacheAndRoute(self.__WB_MANIFEST);
-cleanupOutdatedCaches();
-
-try {
-    const handler = createHandlerBoundToURL('/');
-    const route = new NavigationRoute(handler);
-    registerRoute(route);
-} catch (error) {
-    console.warn('Error while registering cache route', { error });
-}
-
-//Your service-worker code here.
