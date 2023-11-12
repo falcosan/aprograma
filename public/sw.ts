58fafb01d6e365a4fe1f12b25ce65eb6869e311e
daniele.falchetti2 <daniele.falchetti@viewnext.com>
2023-11-12 19:43:08 +0100

    w

--- public/sw.ts
+++ public/sw.ts
@@ -1,5 +1,6 @@
-import { precacheAndRoute } from 'workbox-precaching'
+import { clientsClaim } from 'workbox-core'
 
 declare let self: ServiceWorkerGlobalScope
 
-precacheAndRoute(self.__WB_MANIFEST || [])
+self.skipWaiting()
+clientsClaim()
