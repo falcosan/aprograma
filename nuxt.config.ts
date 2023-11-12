ee415fe5b11eeec2629097aa272131aee53339fa
daniele.falchetti2 <daniele.falchetti@viewnext.com>
2023-11-12 21:28:53 +0100

    wq

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -83,7 +83,7 @@ export default defineNuxtConfig({
     registerType: 'autoUpdate',
     workbox: {
       navigateFallback: null,
-      globPatterns: ['**/*.{js,html,css}'],
+      globPatterns: ['**/*.{js,css}', '**/index.html'],
       globIgnores: ['sw.js', 'workbox-*.js']
     }
   },
