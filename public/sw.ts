375f4eb64ea40d30be4e9dd1f82da7deb1e76925
daniele.falchetti2 <daniele.falchetti@viewnext.com>
2023-11-12 19:30:05 +0100

    g

--- public/sw.ts
+++ public/sw.ts
@@ -2,6 +2,5 @@ import { cleanupOutdatedCaches, precacheAndRoute } from 'workbox-precaching'
 
 declare let self: ServiceWorkerGlobalScope
 
-cleanupOutdatedCaches()
 
 precacheAndRoute(self.__WB_MANIFEST)
