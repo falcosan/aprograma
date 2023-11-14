b9c20d3bf6eeb158c3a8fa6fe673b1b18679ab07
daniele.falchetti@viewnext.com <daniele.falchetti@viewnext.com>
2023-11-14 09:47:43 +0100

    a

--- pages/portfolio/[slug].vue
+++ pages/portfolio/[slug].vue
@@ -3,17 +3,13 @@ const route = useRoute();
 const { locale } = useI18n();
 const { seoDynamic } = useSeo();
 const config = useRuntimeConfig();
-const { data: project } = await useAsyncData(
-  route.params.slug,
-  async () => {
-    const { story } = await $fetch('/api/storyblok', {
-      headers: { 'x-auth': config.public.envXAuth },
-      params: { slug: `portfolio/${route.params.slug}`, lang: locale.value }
-    });
-    return story;
-  },
-  { watch: [locale] }
-);
+const { data: project } = await useAsyncData(route.params.slug, async () => {
+  const { story } = await $fetch('/api/storyblok', {
+    headers: { 'x-auth': config.public.envXAuth },
+    params: { slug: `portfolio/${route.params.slug}`, lang: locale.value }
+  });
+  return story;
+});
 watch(project, val => seoDynamic(val), { immediate: true });
 </script>
 
