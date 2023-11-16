e2bc0140fca2ca6826591763ccc7d8a1728114f2
daniele.falchetti@viewnext.com <daniele.falchetti@viewnext.com>
2023-11-16 12:05:00 +0100

    d

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -84,7 +84,7 @@ export default defineNuxtConfig({
     strategies: 'injectManifest',
     injectManifest: {
       globIgnores: ['sw.js', 'workbox-*.js'],
-      globPatterns: ['**/*.{css,png,jpg,jpeg,svg,ico}']
+      globPatterns: ['**/*.{js,css,png,jpg,jpeg,svg,ico}']
     }
   },
   robots: {
