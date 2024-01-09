723add6c18be58e9ebbd7d98a8ccfc41199eef63
daniele.falchetti2 <daniele.falchetti@viewnext.com>
2024-01-09 17:51:23 +0100

    d

--- layouts/empty.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <main class="aprograma-theme"><slot /></main>
-</template>
--- pages/note/[slug].vue
+++ pages/note/[slug].vue
@@ -1,10 +1,9 @@
 <script setup>
 const route = useRoute();
-definePageMeta({ layout: 'empty' });
 const { fetcher } = useFetcher({ slug: `note/${route.params.slug}` });
 const { data: note } = await useAsyncData('note', fetcher);
 </script>
 
 <template>
-  <StoryblokComponent :key="note.id" :blok="{ ...note.content, id: note.id }" />
+  <StoryblokComponent :key="note.id" :blok="{ ...note.content, id: note.id, title: note.name }" />
 </template>
