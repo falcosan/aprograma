317499d19416058c3cdda305994ae200080771e1
daniele.falchetti2 <daniele.falchetti@viewnext.com>
2024-01-09 11:25:37 +0100

    sfd

---
+++ pages/note/[slug].vue
@@ -0,0 +1,9 @@
+<script setup>
+const route = useRoute();
+const { fetcher } = useFetcher({ slug: `note/${route.params.slug}` });
+const { data: note } = await useAsyncData('note', fetcher);
+</script>
+
+<template>
+  <StoryblokComponent :key="note.id" :blok="{ ...note.content, id: note.id }" />
+</template>
---
+++ storyblok/global/Note.vue
@@ -0,0 +1,27 @@
+<template>
+  <div
+    class="note-body w-full p-5 rounded"
+    :style="`background-color:${$binaryControl(blok.background_color, 'color', '#e0e0e0')};`"
+  >
+    <article
+      :style="`color: ${$binaryControl(blok.text_color, 'color')};`"
+      class="note-article markdown block mb-5"
+      v-html="markdownToHtml(blok.long_text)"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    blok: {
+      type: Object,
+      required: true
+    }
+  },
+  setup() {
+    const { markdownToHtml } = useMarkdown();
+    return { markdownToHtml };
+  }
+};
+</script>
