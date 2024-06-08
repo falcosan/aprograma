c7774796c53d83aa3e1dcb29d50ebf02b248169f
Daniele Falchetti <danielefalche@gmail.com>
2024-06-08 18:55:51 +0200

    f

--- composables/useScreen.js
+++ composables/useScreen.js
@@ -1,27 +1,11 @@
-import {
-  useWindowSize,
-  useElementSize,
-  useWindowScroll,
-  useMutationObserver,
-  useDocumentVisibility
-} from '@vueuse/core';
+import { useWindowSize, useElementSize } from '@vueuse/core';
 
 export const useScreen = () => {
-  const visibility = useDocumentVisibility();
   const { width: windowWidth } = useWindowSize();
-  const { y: scrollPosition } = useWindowScroll();
-  const windowLoad = computed(
-    () => typeof window === 'undefined' && String(windowWidth.value).toLowerCase() === 'infinity'
-  );
   const elementSize = element => useElementSize(element);
-  const elementObserver = (element, callback, config = { attributes: true }) =>
-    useMutationObserver(element, mutations => callback(mutations), config);
   return {
-    windowLoad,
-    visibility,
     windowWidth,
     elementSize,
-    scrollPosition,
-    elementObserver
+    scrollPosition
   };
 };
