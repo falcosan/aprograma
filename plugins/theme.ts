a9617b695fc2432833de6b8553e334f1095d1a9f
Daniele Falchetti <danielefalche@gmail.com>
2024-11-13 13:16:00 +0100

    g

---
+++ plugins/theme.ts
@@ -0,0 +1,24 @@
+import { useColorMode } from '@vueuse/core'
+
+export default defineNuxtPlugin(() => {
+  const storageModeKey = 'ap_mode'
+  const storageMode = useCookie(storageModeKey, {
+    sameSite: 'lax',
+    default: () => 'light'
+  })
+
+  useServerHead({ htmlAttrs: { class: storageMode.value } })
+
+  return {
+    provide: {
+      mode: useColorMode({
+        storageKey: 'ap_mode_cross_tab',
+        initialValue: storageMode.value,
+        onChanged: (val, defaultHandler) => {
+          storageMode.value = val
+          defaultHandler(val)
+        }
+      })
+    }
+  }
+})
