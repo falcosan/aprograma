745132362acd31ca33b9ca69c94398378790a90b
Daniele Falchetti <danielefalche@gmail.com>
2024-11-21 10:53:34 +0100

    fa

--- layouts/blank.vue
+++ layouts/blank.vue
@@ -3,12 +3,12 @@ import MainComponent from '@/storyblok/layout/Main'
 import HeaderComponent from '@/storyblok/layout/Header'
 const { locale } = useI18n()
 const { seoLayout } = useSeo()
-const { data: layout, status } = await useFetcher('layout', { watcher: true })
+const layout = await useAsyncStoryblok('layout', { language: locale.value })
 watch(locale, (val) => seoLayout({ language: val }), { immediate: true })
 </script>
 
 <template>
-  <Body v-if="status === 'success'">
+  <Body v-if="layout">
     <HeaderComponent
       :blok="$contentByName(layout.content.body, 'Header')[0]"
       layout="blank"
