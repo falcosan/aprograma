f7c9a4fd3956729ce1c18ee4eb14260f1fa79510
Daniele Falchetti <danielefalche@gmail.com>
2024-11-25 18:37:02 +0100

    v

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -44,6 +44,9 @@ export default defineNuxtConfig({
     defaultLocale: 'en',
     detectBrowserLanguage: {
       useCookie: false
+    },
+    experimental: {
+      localeDetector: 'localeDetector.ts'
     }
   },
 
