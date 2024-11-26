90876165836a757a8f2d4e2f712ca6b36606ed4b
Daniele Falchetti <danielefalche@gmail.com>
2024-11-26 17:29:34 +0100

    f

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,19 +1,13 @@
-import { Data, ENV } from './schema/enums'
+import { ENV } from './schema/enums'
 
 export default defineNuxtConfig({
-  app: {
-    rootId: 'aprograma'
-  },
-
-  runtimeConfig: {
-    ...ENV.Private,
-    public: { ...ENV.Public }
-  },
+  app: { rootId: 'aprograma' },
+  runtimeConfig: { ...ENV.Private, public: { ...ENV.Public } },
 
   css: [
-    '~/assets/css/tailwind.css',
     '~/assets/css/main.css',
-    '~/assets/css/theme.css'
+    '~/assets/css/theme.css',
+    '~/assets/css/tailwind.css'
   ],
 
   modules: [
@@ -26,57 +20,22 @@ export default defineNuxtConfig({
     '@storyblok/nuxt',
     '@nuxtjs/tailwindcss'
   ],
-
   image: {
     provider: 'storyblok',
-    storyblok: {
-      baseURL: 'https://a.storyblok.com'
-    }
+    storyblok: { baseURL: 'https://a.storyblok.com' }
   },
-
   i18n: {
-    locales: Object.values(Data.rss)
-      .map((item) => {
-        if (item instanceof Object) return item.language
-        return ''
-      })
-      .filter((item) => typeof item === 'string'),
     defaultLocale: 'en',
-    detectBrowserLanguage: {
-      useCookie: false
-    }
+    locales: ['en', 'es', 'it'],
+    detectBrowserLanguage: { useCookie: false }
   },
-  storyblok: {
-    accessToken: process.env.NUXT_ENV_DUMMY_TOKEN
-  },
+  storyblok: { accessToken: process.env.NUXT_ENV_DUMMY_TOKEN },
 
   sourcemap: true,
+  vite: { build: { chunkSizeWarningLimit: 1000 } },
+  nitro: { compressPublicAssets: true, prerender: { crawlLinks: true } },
 
-  vite: {
-    build: {
-      chunkSizeWarningLimit: 1000
-    }
-  },
-
-  nitro: {
-    compressPublicAssets: true,
-    future: { nativeSWR: true },
-    prerender: {
-      crawlLinks: true
-    }
-  },
-
-  routeRules: {
-    '/**': { isr: 60 }
-  },
-
-  devtools: {
-    enabled: false
-  },
-
-  telemetry: {
-    enabled: false
-  },
-
+  devtools: { enabled: false },
+  telemetry: { enabled: false },
   compatibilityDate: '2024-11-26'
 })
