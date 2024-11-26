d4f294b72161554c24d8946d59d67d2664cbdd08
Daniele Falchetti <danielefalche@gmail.com>
2024-11-26 22:30:43 +0100

    s

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -26,12 +26,7 @@ export default defineNuxtConfig({
   },
   i18n: {
     defaultLocale: 'en',
-    locales: ['en', 'es', 'it'],
-    detectBrowserLanguage: {
-      useCookie: true,
-      redirectOn: 'root',
-      cookieKey: 'ap_language'
-    }
+    locales: ['en', 'es', 'it']
   },
   storyblok: { accessToken: process.env.NUXT_ENV_DUMMY_TOKEN },
 
